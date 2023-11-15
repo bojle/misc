a0a3e0cb5f5c5aaf938af9fc48e643e6de3d85b9
Shreeyash Pandey <shreeyash335@gmail.com>
2023-11-15 12:12:10 +0530

    cpp: update crtp
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- cpp/crtp.cpp
+++ cpp/crtp.cpp
@@ -16,7 +16,28 @@ class Derived: public Base<Derived> {
     }
 };
 
+template <typename Child>
+class SomeTemplateClass {
+    public:
+        bool bar(SomeTemplateClass &x, SomeTemplateClass &y) {
+            return static_cast<Child*>(this)->foo(
+                        static_cast<Child &>(x), static_cast<Child &>(y));
+        }
+};
+
+class Deriving: public SomeTemplateClass<Deriving> {
+    int data;
+    public:
+    Deriving(int _data): data {_data} {
+    }
+    bool foo(Deriving &x, Deriving &y) {
+        return x.data == y.data;
+    }
+};
+
 int main() {
-    Derived d;
-    d.interface();
+    SomeTemplateClass<Deriving> b;
+    Deriving c(23);
+    Deriving a(23);
+    std::cout << b.bar(a,c);
 }
